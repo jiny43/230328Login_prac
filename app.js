309ac4b3d3a2b31ee3ab4f665af5bc2be6cc7f72
jieun <je_eun2735@naver.com>
2023-03-28 11:40:10 +0900

    views 연결

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
+// 이 파일은 프로젝트의 진입점 entry point = 앱파일
+
 const express = require('express')
-const expressHandlebars = require('express-handlebars')
-const bodyParser = require('body-parser')
-const multiparty = require('multiparty')
-
+const expressHandlebars =require('express-handlebars')
 const app = express()
-const port = process.env.PORT || 3000
+const port =process.env.PORT || 3000
 
-if(require.main === module) {
-  app.listen(port, () => {
-    console.log( `Express started on http://localhost:${port}` +
-      '; press Ctrl-C to terminate.' )
-  })
-} else {
-  module.exports = app
-}
+
+//static 미들웨어 추가
+app.use(express.static(__dirname + '/public'))
+//핸들바 뷰 엔진 설정
+app.engine('handlebars', expressHandlebars({
+  defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
+//홈페이지 레이아웃 
+app.get('/',(req,res)=> res.render('home'))
+//로그인 레이아웃 
+app.get('/login',(req,res)=>res.render('login'))
+//회원가입 레이아웃
+app.get('/signup',(req,res)=>res.render('signup'))
+
+app.listen(port, () =>console.log(
+  `Express started on http://localhost:${port};` +
+  `press 컨트롤C ㅎㅎ`))
+
